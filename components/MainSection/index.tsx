4a6b7a4c47ded801124f28e06f4bdff8e30b78f7
Arsen <arsjj7@gmail.com>
2024-01-04 21:59:00 +0400

    change main section description

--- components/MainSection/index.tsx
+++ components/MainSection/index.tsx
@@ -11,7 +11,7 @@ export default function MainSection() {
       <div className="relative pt-40 flex max-md:flex-col-reverse items-center gap-20 max-lg:gap-10 max-w-[1000px] p-10 pb-0 max-md:px-0">
         <div className="relative p-5 max-lg:px-10 md:rounded-3xl bg-black/50 text-lg max-md:text-center">
           <div className="relative z-0 text-white italic">
-            <p className="text-xl mb-5">Hi, my name is Arsen)</p>
+            <p className="text-xl mb-5">Hi, my name is Arsen &#41;</p>
             <p className="text-orange pb-5 border-b-1 border-white/50">
               Welcome to my page!
             </p>
