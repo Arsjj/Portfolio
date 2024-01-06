2d99463c575330b120aedcdb86604001a1bea825
Arsen <arsjj7@gmail.com>
2024-01-06 18:47:38 +0400

    create opentab function

--- components/Works/Card/index.tsx
+++ components/Works/Card/index.tsx
@@ -11,39 +11,44 @@ type Props = {
 };
 
 export default function CardItem({ name, description, src, link }: Props) {
+  const openTab = (link: string) => {
+    window.open(`${link}`, "_blank");
+  };
+
   return (
-    <Link target="_blank" href={link} rel="noopener noreferrer" legacyBehavior>
-      <div className="w-80 cursor-pointer hover:scale-[1.07] duration-250">
-        <div className="relative w-full grid rounded-2xl overflow-hidden">
-          <div className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-white bg-black/50 rounded-r-lg p-2 px-3 font-medium">
-              {name}
-            </p>
-          </div>
-          <div className="relative w-full h-[200px] rounded-lg">
-            <Image
-              fill
-              alt={name}
-              className="z-0 object-cover"
-              src={src}
-              priority
-              sizes="(max-width: 768px) 50px, (min-width: 768px) 300px"
-            />
-          </div>
-          <div className="relative flex w-full items-center h-14 p-2 bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-            <div className="flex flex-grow gap-2 items-center">
-              <div className="flex flex-col">
-                <p className="text-tiny text-white/80">{description}</p>
-              </div>
+    <div
+      className="w-80 cursor-pointer hover:scale-[1.07] duration-250"
+      onClick={() => openTab(link)}
+    >
+      <div className="relative w-full grid rounded-2xl overflow-hidden">
+        <div className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-white bg-black/50 rounded-r-lg p-2 px-3 font-medium">
+            {name}
+          </p>
+        </div>
+        <div className="relative w-full h-[200px] rounded-lg">
+          <Image
+            fill
+            alt={name}
+            className="z-0 object-cover"
+            src={src}
+            priority
+            sizes="(max-width: 768px) 50px, (min-width: 768px) 300px"
+          />
+        </div>
+        <div className="relative flex w-full items-center h-14 p-2 bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+          <div className="flex flex-grow gap-2 items-center">
+            <div className="flex flex-col">
+              <p className="text-tiny text-white/80">{description}</p>
             </div>
-            <Link target="_blank" href={link}>
-              <Button radius="full" size="sm">
-                Open
-              </Button>
-            </Link>
           </div>
+          <Link target="_blank" href={link}>
+            <Button radius="full" size="sm">
+              Open
+            </Button>
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
--- components/Works/index.tsx
+++ components/Works/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 import { motion } from "framer-motion";
@@ -7,14 +7,19 @@ import { data } from "./Data";
 
 export default function Works() {
   return (
-    <div id="projects" className="relative overflow-hidden grid w-full justify-items-center pb-28">
+    <div
+      id="projects"
+      className="relative overflow-hidden grid w-full justify-items-center pb-28"
+    >
       <motion.div
         initial={{ opacity: 0, x: 70 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.5 }}
         viewport={{ once: true }}
       >
-        <p className="text-2xl text-orange text-center font-semibold">Projects</p>
+        <p className="text-2xl text-orange text-center font-semibold">
+          Projects
+        </p>
         <div className="flex flex-wrap pt-10 md:px-10 gap-5 mx-auto justify-center w-full max-w-screen-[1800px]">
           {data.map((item) => (
             <CardItem
