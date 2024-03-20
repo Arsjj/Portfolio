2dfc24da7bd189d01a7d7bf32654c40ce3ae2a8d
Arsen Abrahamyan <arsjj7@gmail.com>
2024-03-20 20:21:57 +0400

    style changes

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -115,7 +115,7 @@ const Contact = () => {
               name="name"
               value={form.name}
               onChange={handleChange}
-              className="bg-tertiary py-4 px-6 border border-[#ffffff14] focus:border-white/70 duration-200 placeholder:text-[#ffffff14] text-white rounded-lg outline-none font-medium"
+              className="bg-tertiary py-4 px-6 border border-[#ffffff14] focus:border-white/70 duration-200 text-white rounded-lg outline-none font-medium"
             />
           </label>
           <label className="flex flex-col">
@@ -125,7 +125,7 @@ const Contact = () => {
               name="email"
               value={form.email}
               onChange={handleChange}
-              className="bg-tertiary py-4 px-6 border border-[#ffffff14] focus:border-white/70 duration-200 placeholder:text-secondary/30 text-white rounded-lg outline-none font-medium"
+              className="bg-tertiary py-4 px-6 border border-[#ffffff14] focus:border-white/70 duration-200 text-white rounded-lg outline-none font-medium"
             />
           </label>
           <label className="flex flex-col">
@@ -135,7 +135,7 @@ const Contact = () => {
               name="message"
               value={form.message}
               onChange={handleChange}
-              className="bg-tertiary py-4 px-6 border border-secondary/20 focus:border-white/70 duration-200 placeholder:text-secondary/30 text-white rounded-lg outline-none font-medium"
+              className="bg-tertiary py-4 px-6 border border-secondary/20 focus:border-white/70 duration-200 text-white rounded-lg outline-none font-medium"
             />
           </label>
 
