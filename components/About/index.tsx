0796d13d540eaf891bbb41cdea600a44a1970739
Arsen Abrahamyan <arsjj7@gmail.com>
2024-06-08 12:48:01 +0400

    change about cards animation

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -19,8 +19,12 @@ const ServiceCard = ({ index, name, icon }: { index: number, name: string, icon:
     }}
   >
     <motion.div
-      // variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      initial={{ opacity: 0, x: -70 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+
     >
       <div className="bg-tertiary rounded-[20px] py-5  min-h-[170px] max-sm:min-h-[130px] max-xsm-[min-h-100px] flex justify-evenly items-center flex-col">
         <Image
@@ -60,26 +64,19 @@ const About = () => {
         applications and user interfaces. Proficient in JavaScript and Typescript,
         with hands-on experience in modern frontend technologies such as React and Next JS.
         Additionally, skilled in backend development using Node.js, enabling the creation of robust server-side applications and APIs.
-        <br/>
+        <br />
         Aiming to take on new challenges, learn more and use my coding skills for developing
         new features and contribute to successful projects.
       </motion.p>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-8 font-semibold text-white sm:text-xl">Technologies I have worked with</motion.p>
-      <motion.div className="w-full max-w-5xl mx-auto mt-10 flex flex-wrap justify-center gap-10 max-sm:gap-5 max-xsm:gap-4"
-        // variants={fadeIn("right", "spring")}
-        // variants={fadeIn("", "", 0.1, 1)}
-        initial={{ opacity: 0, x: -70 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.5 }}
-        viewport={{ once: true }}
-
+      <div className="w-full max-w-5xl mx-auto mt-10 flex flex-wrap justify-center gap-10 max-sm:gap-5 max-xsm:gap-4"
       >
         {technologies.map((service, index) => (
           <ServiceCard key={service.name} index={index} {...service} />
         ))}
-      </motion.div>
+      </div>
 
 
     </>
